9c595b8fd284d001ff5597c55c4f17c344754573
Altinisik nejdet <nejdet.altinisik@yahoo.fr>
2025-11-19 04:30:08 +0100

    Create Model960.hlsl

---
+++ Model960.hlsl
@@ -0,0 +1,7 @@
+//topological anomalies would appear in low average gpus compute.
+//so we czn use hugh neegy systems algebraic znd canonical vlues at one time here.
+//notice we sokt need other properties. so caninical themodynalics is enoguh
+//.
+
+/clearcoar
+void canonicalhundreadoneaverzge(){}
